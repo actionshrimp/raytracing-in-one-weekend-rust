2619b62c205db64a69d7adab0894ebb5b21c78ad
Dave Aitken <dave.aitken@gmail.com>
2020-12-22 22:07:51 +0000

    more vector helpers, setting up camera

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,18 @@ impl Vec {
         Vec { x: x, y: y, z: z }
     }
 
+    fn mul(&self, t : f64) -> Vec {
+        Vec::new(self.x * t, self.y * t, self.z * t)
+    }
+
+    fn add(&self, v: Vec) -> Vec {
+        Vec::new(self.x + v.x, self.y + v.y, self.z + v.z)
+    }
+
+    fn sub(&self, v: &Vec) -> Vec {
+        Vec::new(self.x - v.x, self.y - v.y, self.z - v.z)
+    }
+
     fn write(&self) -> () {
         let ir = (255.999 * self.x).round() as i32;
         let ig = (255.999 * self.y).round() as i32;
@@ -20,13 +32,56 @@ impl Vec {
         println!("{} {} {}", ir, ig, ib)
     }
 
+    fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    fn unit(&self) -> Vec {
+        let l = self.length();
+        self.mul(1.0 / l)
+    }
+
+}
+
+
+type Point = Vec;
+type Color = Vec;
+
+
+struct Ray<'a> {
+    origin: &'a Vec,
+    direction: &'a Vec
+}
+
+fn ray_color(r: &Ray) -> Color {
+    let u = r.direction.unit();
+    let t = 0.5 * (u.y + 1.0);
+    let v1 = Vec::new(1.0, 1.0, 1.0).mul(1.0 - t);
+    let v2 = Vec::new(0.5, 0.7, 1.0).mul(t);
+
+    v1.add(v2)
 }
 
 fn main() {
+    let aspect_ratio: f64 = 16.0 / 9.0;
     let image_width: u16 = 256;
-    let image_height: u16 = 256;
+    let image_height: u16 = (image_width as f64 / aspect_ratio) as u16;
     let rgb_max: u8 = 255;
 
+    //camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vec::new(0.0, 0.0, 0.0);
+    let horizontal = Vec::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec::new(0.0, viewport_height, 0.0);
+    let deep = Vec::new(0.0, 0.0, focal_length);
+    let lower_left_corner = origin.sub(&horizontal.mul(1.0 / 2.0)).sub(&vertical.mul(1.0 / 2.0)).sub(&deep);
 
     println!("P3");
     println!("{} {}", image_width, image_height);
@@ -35,11 +90,13 @@ fn main() {
 
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let r = (i as f64) / ((image_width - 1) as f64);
-            let g = (j as f64) / ((image_height - 1) as f64);
-            let b: f64 = 0.25;
+            let u = (i as f64) / ((image_width - 1) as f64);
+            let v = (j as f64) / ((image_height - 1) as f64);
+            let direction = lower_left_corner.add(horizontal.mul(u)).add(vertical.mul(v)).sub(&origin);
 
-            Vec::new(r, g, b).write();
+            let r = Ray { origin: &origin, direction: &direction };
+            let pixel_color = ray_color(&r);
+            pixel_color.write()
         }
     }
 }
