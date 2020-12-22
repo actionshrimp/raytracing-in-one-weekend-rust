a1247f5f81707b97b375b2e8316d7b05aa6b370d
Dave Aitken <dave.aitken@gmail.com>
2020-12-22 15:49:27 +0000

    vec helper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct Vec {
+    x: f64,
+    y: f64,
+    z: f64
+}
+
+impl Vec {
+    fn zero() -> Vec {
+        Vec { x: 0.0, y: 0.0, z: 0.0 }
+    }
+
+    fn new(x: f64, y: f64, z: f64) -> Vec {
+        Vec { x: x, y: y, z: z }
+    }
+
+    fn write(&self) -> () {
+        let ir = (255.999 * self.x).round() as i32;
+        let ig = (255.999 * self.y).round() as i32;
+        let ib = (255.999 * self.z).round() as i32;
+        println!("{} {} {}", ir, ig, ib)
+    }
+
+}
+
 fn main() {
     let image_width: u16 = 256;
     let image_height: u16 = 256;
@@ -11,15 +35,11 @@ fn main() {
 
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let r = (i as f32) / ((image_width - 1) as f32);
-            let g = (j as f32) / ((image_height - 1) as f32);
-            let b: f32 = 0.25;
+            let r = (i as f64) / ((image_width - 1) as f64);
+            let g = (j as f64) / ((image_height - 1) as f64);
+            let b: f64 = 0.25;
 
-            let ir = (255.999 * r).round() as i32;
-            let ig = (255.999 * g).round() as i32;
-            let ib = (255.999 * b).round() as i32;
-
-            println!("{} {} {}", ir, ig, ib);
+            Vec::new(r, g, b).write();
         }
     }
 }
